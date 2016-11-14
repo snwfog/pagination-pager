c53429469c43a3f8d4faa02558c0b6f09199af09
Charles Yang <charles.y.yang@oracle.com>
2016-11-14 10:12:12 -0500

    Remove unused

--- Brocfile.js
+++ Brocfile.js
@@ -19,14 +19,14 @@ var app = new EmberAddon();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+//app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 //app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
 // Copy glyphicon fonts
-var fonts = pickFiles('bower_components/bootstrap/dist/fonts', {
-  srcDir: '/',
-  files: ['**/*'],
-  destDir: '/fonts'
-});
+// var fonts = pickFiles('bower_components/bootstrap/dist/fonts', {
+//   srcDir: '/',
+//   files: ['**/*'],
+//   destDir: '/fonts'
+// });
 
-module.exports = mergeTrees([app.toTree(), fonts]);
+module.exports = mergeTrees([app.toTree()]);
